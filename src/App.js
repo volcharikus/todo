4926ca351a8087c399dd4e68432dcee9176ddbf7
admin <admin@MacBook-Pro-admin.local>
2023-01-20 12:43:13 +0200

    first push

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import {Todo} from "./components/common";
+
+function App() {
+  return (
+    <div className="App">
+<Todo/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/common/Todo/index.js
@@ -0,0 +1,14 @@
+import "./index.scss"
+
+import React from 'react';
+const Todo = () => {
+    return (
+        <div>
+<h1>fsafasfasfafssaf
+Sanek perets
+</h1>
+        </div>
+    );
+};
+
+export default Todo;
